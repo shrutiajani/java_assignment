af4237f778d19d4e9eda7b243247f54fda178a8d
shrutiajani <ajanishruti366@gmail.com>
2025-12-06 16:36:08 +0530

    Add files via upload

---
+++ naturalnumber.java
@@ -0,0 +1,20 @@
+package p1;
+
+
+public class naturalnumber {
+
+	public static void main(String[] args) {
+	
+		
+		int i=0;
+		
+		 while(i<=10)
+		 {
+			
+			 System.out.println( i );
+			 i++;
+		 }
+		
+	}
+
+}
